b5eccc754619a877d96f5d26316aa2ab73d7a2da
koladev <onaelmangabo@gmail.com>
2022-11-12 18:59:57 +0100

    🔧 update menu module config

--- src/menus/menus.module.ts
+++ src/menus/menus.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MenusService } from './menus.service';
 import { MenusController } from './menus.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Menu, MenuSchema } from './schemas/menu.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Menu.name,
+        schema: MenuSchema,
+      },
+    ]),
+  ],
   controllers: [MenusController],
-  providers: [MenusService]
+  providers: [MenusService],
 })
 export class MenusModule {}
