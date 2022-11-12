7dc5f6e618485d247db9fd309dce65e75609edac
koladev <onaelmangabo@gmail.com>
2022-11-12 18:18:08 +0100

    ✨ add mongodb configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/Restaurant')],
   controllers: [AppController],
   providers: [AppService],
 })
