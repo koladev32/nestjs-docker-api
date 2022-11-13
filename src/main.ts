2bd1baebcd22c3361b47c62786283afc9b929835
koladev <onaelmangabo@gmail.com>
2022-11-13 12:33:04 +0100

    ✨ white list non used fields

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(3000);
 }
