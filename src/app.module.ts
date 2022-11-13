a17798da8d07663d2ac59dda213560ae0f99685e
koladev <onaelmangabo@gmail.com>
2022-11-13 13:43:19 +0100

    🔧 add env files configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { MenusModule } from './menus/menus.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/Restaurant'),
-    MenusModule,
-  ],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URL), MenusModule],
   controllers: [AppController],
   providers: [AppService],
 })
