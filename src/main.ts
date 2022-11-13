133ea8d2c98c975d6aa3ff30d28a346e2c1ec284
koladev <onaelmangabo@gmail.com>
2022-11-13 15:43:04 +0100

    ✨ add dockerigore file

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  await app.listen(3000);
+  await app.listen(process.env.APP_PORT);
 }
 bootstrap();
